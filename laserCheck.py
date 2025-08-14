a0e6433eea8e24f088527979ccd1255999c2fbc7
Filip Koszut <pecet003@gmail.com>
2025-08-14 13:42:50 +0200

    edit Gui

--- laserCheck.py
+++ laserCheck.py
@@ -1,102 +1,105 @@
+from textual.app import App, ComposeResult
+from textual.containers import Container, VerticalScroll
+from textual.widgets import Static, Button, Input
+from textual import on
+import asyncio
+
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 import time
-from textual.app import App, ComposeResult
-from textual.widgets import Static, Input, Button
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 
-class LaserChecker:
-    def __init__(self):
-        self.driver = webdriver.Chrome()
-        self.driver.implicitly_wait(30)
+class MyLog(Static):
+    def on_mount(self):
+        self.lines = []
 
-    def skeleton(self, address: str):
-        table = [500, 1000, 1500, 2000]
-        self.realValue = []
-
-        self.driver.get(f"http://{address}:3000/#/")  # use passed address
-        self.driver.find_element(By.LINK_TEXT, "Controls").click()
-        self.driver.find_element(By.LINK_TEXT, "XY Panel").click()
-        self.driver.find_element(
-            By.CSS_SELECTOR,
-            "#sinterit_web > div > div.position-relative > div.container.position-relative > div > div > div > div > ul > div:nth-child(1) > li:nth-child(1) > div:nth-child(1) > div:nth-child(4) > div > button"
-        ).click()
-        self.laser(table)
-
-    def laser(self, table):
-        powerOverride = self.driver.find_element(
-            By.CSS_SELECTOR,
-            "#sinterit_web > div > div.position-relative > div.container.position-relative > div > div > div > div > ul > div:nth-child(1) > li:nth-child(3) > div > div:nth-child(2) > input"
-        )
-        for x in table:
-            print(x)
-            powerOverride.send_keys(Keys.CONTROL, "a")
-            powerOverride.send_keys(x)
-            self.driver.find_element(
-                By.CSS_SELECTOR,
-                "#sinterit_web > div > div.position-relative > div.container.position-relative > div > div > div > div > ul > div:nth-child(1) > li:nth-child(3) > div > div:nth-child(4) > button"
-            ).click()
-            self.realValue.append(input("Value from gauge: "))
-
-            stat = statistic(self.realValue, table)      
-            prec = stat.prec() 
-        print("AVG:[%] " + prec)
-        print("Sum AVG[%] "+stat.avgPrec(prec))        
-            
-        time.sleep(3)
-
-        
+    def write(self, text: str):
+        self.lines.append(text)
+        self.update("\n".join(self.lines))
 
 
-class TextualGui(App):
+class TerminalAppGui(App):
+    CSS = """
+    Screen { layout: vertical; }
+    #top { height: 3; background: darkgreen; text-align: center; text-style: bold; }
+    #main { layout: horizontal; }
+    #left { width: 30%; background: #444; align: center middle; }
+    #right { width: 70%; background: #222; }
+    #bottom { height: 3; background: darkblue; text-align: center; }
+    Button, Input { width: 90%; margin: 1; }
+    """
 
     def compose(self) -> ComposeResult:
-        self.static = Static("⚡ Laser Semi Manual Tester ⚡")
-        yield self.static
-        self.address_input = Input(placeholder="IP address of printer", id="ip_input")
-        yield self.address_input
-        yield Button("Turn Laser On", id="laser_button")
+        yield Static("⚡ LisaX laser checker ⚡", id="top")
 
-    def on_mount(self):
-        self.static.styles.background = "blue"
-        self.static.styles.border = ("solid", "white")
-        self.static.styles.text_align = "center"
-        self.static.styles.padding = (1, 1)
-        self.static.styles.margin = (4, 4)
+        with Container(id="main"):
+            with VerticalScroll(id="left"):
+                yield Input(placeholder="IP address of printer!", id="ip_input")
+                yield Button("Run Laser !", id="run_btn")
 
-    def on_button_pressed(self, event: Button.Pressed) -> None:
-        if event.button.id == "laser_button":
-            ip_address = self.address_input.value.strip()
-            if ip_address:
-                laser = LaserChecker()
-                laser.skeleton(ip_address)
-            else:
-                print("No IP address entered!")
+            with VerticalScroll(id="right"):
+                yield MyLog(id="log_panel")
 
-class statistic:
-    def __init__(self, real, given):
-        self.real = real
-        self.given = given
-        self.delta = None
-    def difer(self):
-        self.delta = []
-        for i in range(len(self.given)):
-            holder = self.real[i] - self.given[i]
-            self.delta.append(holder)
-    def prec(self):
-        percents = []
-        if self.delta is None:
-            self.difer()
-        for i in range(len(self.delta)):
-            percHolder = (self.delta[i] / self.real[i] * 100)
-            percents.append(percHolder)
-        return(percents)
-    def avgPrec(self, percTab):
-        return(sum(percTab)/len(self.delta))
-        
+        yield Static("by FK dev Squad", id="bottom")
+
+    @on(Button.Pressed, "#run_btn")
+    async def run_laser(self) -> None:
+        log_panel = self.query_one("#log_panel", MyLog)
+        ip = self.query_one("#ip_input", Input).value.strip()
+
+        if not ip:
+            log_panel.write("[ERROR] Nie podano IP!")
+            return
+
+        log_panel.write(f"[START] Uruchamiam laser dla IP: {ip}")
+
+        asyncio.create_task(self.run_selenium(ip, log_panel))
+
+    async def run_selenium(self, ip: str, log_panel: MyLog):
+        await asyncio.to_thread(self.selenium_task, ip, log_panel)
+
+    def selenium_task(self, ip: str, log_panel: MyLog):
+        try:
+            table = [500, 1000, -1]
+            driver = webdriver.Chrome()
+            driver.implicitly_wait(30)
+
+            log_panel.write(f"[INFO] Otwieram stronę: http://{ip}:3000/#/")
+            driver.get(f"http://{ip}:3000/#/")
+
+            driver.find_element(By.LINK_TEXT, "Controls").click()
+            driver.find_element(By.LINK_TEXT, "XY Panel").click()
+            driver.find_element(By.CSS_SELECTOR,
+                "#sinterit_web > div > div.position-relative > div.container.position-relative > "
+                "div > div > div > div > ul > div:nth-child(1) > li:nth-child(1) > div:nth-child(1) "
+                "> div:nth-child(4) > div > button"
+            ).click()
+
+            powerOverride = driver.find_element(By.CSS_SELECTOR,
+                "#sinterit_web > div > div.position-relative > div.container.position-relative > "
+                "div > div > div > div > ul > div:nth-child(1) > li:nth-child(3) > div > div:nth-child(2) > input"
+            )
+
+            for x in table:
+                log_panel.write(f"[ACTION] Ustawiam moc lasera na: {x}")
+                powerOverride.send_keys(Keys.CONTROL, "a")
+                powerOverride.send_keys(str(x))
+                driver.find_element(By.CSS_SELECTOR,
+                    "#sinterit_web > div > div.position-relative > div.container.position-relative > "
+                    "div > div > div > div > ul > div:nth-child(1) > li:nth-child(3) > div > div:nth-child(4) > button"
+                ).click()
+                time.sleep(3)
+
+            log_panel.write("[DONE] Operacja zakończona, zamykam przeglądarkę...")
+            time.sleep(2)
+            driver.quit()
+
+        except Exception as e:
+            log_panel.write(f"[ERROR] {e}")
 
 
-if __name__ == "__main__":
-    app = TextualGui()
-    app.run()
+if __name__ == '__main__':
+    TerminalAppGui().run()
